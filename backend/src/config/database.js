4405b8e18144412950664a4b0b1bfbc878f12239
Lavish Singhal <lavish2801@gmail.com>
2025-06-06 00:56:25 +0530

    vercel fix

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,28 +1,4 @@
 require('dotenv').config();
+const config = require('./config.json');
 
-module.exports = {
-  development: {
-    url: process.env.SUPABASE_DATABASE_URL,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    logging: false
-  },
-  production: {
-    url: process.env.SUPABASE_DATABASE_URL,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    },
-    logging: false
-  }
-}; 
+module.exports = config; 
